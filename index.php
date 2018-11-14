933e0b13603770dbeebd78f01341e1a731d37fe7
Aluno <aluno@edu.senac.br>
2018-11-14 21:44:37 -0200

    incerção de PHP

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<?php 
+
+$nome="Naldo";
+echo $nome;
+#comentando uma linha
+//comentario em mais uma linha//
+/*comentario em bloco
+dentro do PHP*/
+
+ ?>
+
+</body>
+</html>
