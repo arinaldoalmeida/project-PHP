ee79fed858d66bdbdcffdaac73470f40f67ba654
Aluno <aluno@edu.senac.br>
2018-11-19 22:13:07 -0200

    Atualizações

---
+++ switch.php
@@ -0,0 +1,20 @@
+<?php 
+$cor ="Branco";
+switch ($cor){
+	case "Amarelo":
+	echo "É mararelo igaul ao sol";
+	break;
+	case "Branco":
+	echo "poxa é branco";
+	break;
+	case "Vermelho":
+	echo "Ha,ha, é igual a sangue";
+	break;
+	default:
+	echo "Cor não encontrada";
+	break;
+}
+
+?>
+
+
