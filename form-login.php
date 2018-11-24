195f0ced64a10b0992d3cd0624fde0aae442f25d
Aluno <aluno@edu.senac.br>
2018-11-23 22:12:53 -0200

    sistema loja

--- form-login.php
+++ form-login.php
@@ -6,39 +6,39 @@
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 </head>
 <body>
-<div class="contaner">
-	<p>&nbsp;</p>
-	<p>&nbsp;</p>
-	<div class="row">
-		<div class="col"></div>
-		<div class="col">
+	<div class="contaner">
+		<p>&nbsp;</p>
+		<p>&nbsp;</p>
+		<div class="row">
+			<div class="col"></div>
+			<div class="col">
 
-			<div class="card shadow p-3 mb-5 bg-white rounded">
-				<div class="card-body">
-					<div class="text-center">
-						<img src="img/login.png" width="130px" class="img-fluid">
+				<div class="card shadow p-3 mb-5 bg-white rounded">
+					<div class="card-body">
+						<div class="text-center">
+							<img src="img/login.png" width="130px" class="img-fluid">
+						</div>
+						<h3 class="text-center">Área restrita</h3>
+						<form method="post" action="login.php">
+							<div class="form-group">
+								<label>Login:</label>
+								<input type="text" name="login" class="form-control" placeholder="Digite seu usuário" required>
+							</div>
+							<div class="form-group">
+								<label>Senha:</label>
+								<input type="password" name="senha" class="form-control" placeholder="Digite sua senha" required>
+							</div>
+							<button type="submit" class="btn btn-dark float-right">Logar</button>
+						</form>
 					</div>
-					<h3 class="text-center">Área restrita</h3>
-					<form method="post" action="login.php">
-						<div class="form-group">
-							<label>Login:</label>
-							<input type="text" name="login" class="form-control" placeholder="Digite seu usuário" required>
-						</div>
-						<div class="form-group">
-							<label>Senha:</label>
-							<input type="password" name="senha" class="form-control" placeholder="Digite sua senha" required>
-						</div>
-						<button type="submit" class="btn btn-dark float-right">Logar</button>
-					</form>
 				</div>
 			</div>
+			<div class="col"></div>
 		</div>
-		<div class="col"></div>
+
 	</div>
 
-</div>
-
-<script src="js/jquery-3.3.1.slim.min.js"></script>
+	<script src="js/jquery-3.3.1.slim.min.js"></script>
 	<script type="text/javascript" src="js/bootstrap.min.js"></script>
 </body>
 </html>
---
+++ sistema-loja/produtos.php
@@ -0,0 +1,60 @@
+<?php 
+	//$mysqli = new mysqli("localhost", "usuario", "senha", "database");
+	//nesta linha realizo a conexão
+	$conexao = new mysqli("localhost", "root", "senac", "loja");
+	//nesta linha, monto a consulta a ser realizada
+	$sql_produtos = "SELECT p.*, c.descricao as categoria 
+					 FROM produto p
+					 LEFT JOIN  categoria c ON c.id = p.id_categoria
+					 ORDER BY p.id";
+	//nesta linha, executo a consulta montada
+	$produtos = $conexao->query($sql_produtos);
+?>
+
+<?php include "layout/header.php"; ?>
+<?php include "layout/menu.php"; ?>
+<div class="container">
+	<p>&nbsp;</p>
+	<h1>Produtos</h1>
+	<div class="row">
+		<div class="col">
+			<nav aria-label="breadcrumb">
+			  <ol class="breadcrumb">
+			    <li class="breadcrumb-item"><a href="principal.php">Principal</a></li>
+			    <li class="breadcrumb-item active" aria-current="page">Produtos</li>
+			  </ol>
+			</nav>
+		</div>
+	</div>
+	<div class="row">
+		<table class="table table-bordered table-striped table-hover">
+			<tr>
+				<th>#</th>
+				<th>Descrição</th>
+				<th>Preço</th>
+				<th>Estoque</th>
+				<th>Categoria</th>
+				<th>Ações</th>
+			</tr>
+
+			<?php while($produto = $produtos->fetch_array(MYSQLI_ASSOC)) { //aqui eu starto o loop dos dados da consulta ?>
+				<tr>
+					<td><?php echo $produto['id']; ?></td>
+					<td><?php echo $produto['nome']; ?></td>
+					<td>R$ <?php echo number_format($produto['valor'], 2, ',','.'); ?></td>
+					<td><?php echo $produto['estoque']; ?></td>
+					<td><?php echo $produto['categoria'] ?></td>
+					<td></td>
+				</tr>
+
+			<?php } //aqui finalizo o loop dos dados ?>
+
+		</table>
+		
+		
+
+	</div>
+
+</div>
+
+<?php include "layout/footer.php"; ?>
